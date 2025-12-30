79c58a8daf27834eaf9860228e723ff917cffddc
uday-binary-beats <udayshikhar.das@natureglobal.com>
2025-12-30 16:17:14 +0530

    few changes

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -62,6 +62,7 @@ export default buildConfig({
       connectionString: process.env.DATABASE_URL || '',
     },
   }),
+
   collections: [Pages, Posts, Media, Categories, Users],
   cors: [getServerSideURL()].filter(Boolean),
   globals: [Header, Footer],
